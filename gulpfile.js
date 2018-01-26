cf3d74b6dad69a3d01a2e49c811686a2861ccbe5
Sven van Steenis <sven.vsteenis@gmail.com>
2018-01-26 15:35:05 +0100

    Enh gulpfile: add livereload & fix annoying sass saving error For sass save error, see: https://github.com/olefredrik/FoundationPress/issues/731#issuecomment-275773642

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,42 @@
 'use strict';
 
-const   gulp    = require('gulp'),
-        sass    = require('gulp-sass'),
-        cssmin  = require('gulp-cssnano'),
-        rename  = require('gulp-rename'),
-        prefix  = require('gulp-autoprefixer');
+const   gulp        = require('gulp'),
+        browserSync = require('browser-sync'),
+        wait        = require('gulp-wait'),
+        sass        = require('gulp-sass'),
+        cssmin      = require('gulp-cssnano'),
+        rename      = require('gulp-rename'),
+        prefix      = require('gulp-autoprefixer');
 
 const sassOptions = {
-    outputStyle: 'expanded'
+    outputStyle: 'expanded',
+    includePaths: ['assets/scss']
 };
 
 const prefixOptions = {
     browsers: ['last 2 versions']
 }
 
-function styles() {
-    return gulp.src('assets/sass/main.scss')
-        .pipe(sass(sassOptions))
+gulp.task('styles', function(){
+    return gulp.src('./assets/sass/main.scss')
+        .pipe(wait(500))
+        .pipe(sass(sassOptions).on('error', sass.logError))
         .pipe(prefix(prefixOptions))
-        .pipe(rename('main.css'))
         .pipe(gulp.dest('assets/css'))
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('assets/css'))
-}
+        .pipe(browserSync.reload({stream: true}))
+});
 
-function watch() {
-    gulp.watch('assets/sass/**/*.scss', styles)
-}
+gulp.task('serve', function(){
+    browserSync.init({
+        proxy: 'http://localhost',
+        port: 80,
+        browser: 'chrome'
+    });
 
-gulp.task('default', watch);
+    gulp.watch('assets/sass/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['serve']);
