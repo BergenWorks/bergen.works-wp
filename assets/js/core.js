1a4cf26f41828cf34cbe47d44faf312c586ac322
Sven van Steenis <sven.vsteenis@gmail.com>
2018-01-26 22:14:58 +0100

    Style main navbar & add relevant JS

---
+++ assets/js/core.js
@@ -0,0 +1,17 @@
+jQuery(function($) {
+
+    // Add top margin to compensate for fixed nav
+    var navbarHeight = $('.nav-main').height();
+    $('body').css('margin-top', navbarHeight);
+
+    var mainMenu = $('#main-menu-content');
+    var iconBars = 'fa-bars';
+    var iconClose = 'fa-times';
+
+    mainMenu.on('show.bs.collapse', function(){
+        $('.navbar-toggler > .fa').removeClass(iconBars).addClass(iconClose);
+    });
+    mainMenu.on('hide.bs.collapse', function(){
+        $('.navbar-toggler > .fa').removeClass(iconClose).addClass(iconBars);
+    });
+});
--- parts/nav-main.php
+++ parts/nav-main.php
@@ -1,30 +1,32 @@
-<nav class="navbar navbar-expand-lg">
-    <a href="<?php echo home_url(); ?>" class="navbar-brand">
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/bergenworks-logo-white.png' ?>" alt="" class="navbar-logo">
-    </a>
+<nav class="navbar navbar-expand-md fixed-top nav-main">
+    <div class="container">
+        <a href="<?php echo home_url(); ?>" class="navbar-brand">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/bergenworks-logo-white.png' ?>" alt="" class="navbar-logo">
+        </a>
 
-    <button class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbar-content"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
+        <button class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#main-menu-content"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
 
-        <i class="fa fa-bars"></i>
-    </button>
+            <i class="fa fa-bars"></i>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbar-content">
-        <?php
-            wp_nav_menu(array(
-                'theme_location' => 'main_menu',
-                'menu_id' => 'main-menu',
-                'container' => false,
-                'depth' => 2,
-                'menu_class' => 'navbar-nav ml-auto',
-                'walker' => new Bootstrap_NavWalker(),
-                'fallback_cb' => 'BootStrap_NavWalker::fallback'
-            ));
-        ?>
+        <div class="collapse navbar-collapse" id="main-menu-content">
+            <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'main_menu',
+                    'menu_id' => 'main-menu',
+                    'container' => false,
+                    'depth' => 2,
+                    'menu_class' => 'navbar-nav ml-auto',
+                    'walker' => new Bootstrap_NavWalker(),
+                    'fallback_cb' => 'BootStrap_NavWalker::fallback'
+                ));
+            ?>
+        </div>
     </div>
 </nav>
