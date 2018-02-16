1ddb07fe18952abf0fe2edb6a81d7bb278867451
Sven van Steenis <sven.vsteenis@gmail.com>
2018-02-16 16:42:26 +0100

    adds styling for regular header

--- header.php
+++ header.php
@@ -10,7 +10,16 @@
 <body>
     <?php get_template_part( 'parts/nav', 'main' ); ?>
 
-    <div class="jumbotron jumbotron-fluid header">
+    <?php
+        if($header_img = get_the_post_thumbnail('full')) {
+            $bg_img = 'background:  linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(' . $header_img['url'] . ') no-repeat center center fixed';
+        } else {
+            $header_img = get_stylesheet_directory_uri() . '/assets/images/bergenworks-header-default.png';
+            $bg_img = 'background:  linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(' . $header_img . ') no-repeat center center fixed';
+        }
+    ?>
+
+    <div class="jumbotron jumbotron-fluid header" style="<?php echo $bg_img; ?>; background-size: cover">
         <div class="container header-container">
             <h1 class="header-title"><?php the_title(); ?></h1>
         </div>
